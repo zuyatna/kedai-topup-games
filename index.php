94f0917b37aa01bb55d129bc7dd7f8ec409a263a
Suyatna <zuyatna@gmail.com>
2024-12-18 14:50:55 +0700

    feat: adding static home

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body class="bg-light">
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-mid">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Kedai Topup</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Products</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Portfolio</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact</a>
+                        </li>
+                    </ul>
+                    <button class="btn-user" type="button">
+                        <a href="#"><img src="assets/images/user.png" alt="user" width="20" height="20"></a>
+                    </button>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+        <section style="margin-top: 30px;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6">
+                        <div class="col-lg-7">
+                            <h1>Topup ga ribet, banyak diskonnya!</h1>
+                        </div>
+                        <p style="margin-top: 15px;">
+                            Tempat topup nomor 1 di Indonesia<br>
+                            dengan banyak diskon dan promo menarik<br>
+                            setiap harinya.
+                        </p>
+                        <button class="btn btn-primary" style="margin-top: 15px;">Shop Now</button>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="d-flex justify-content-center">
+                            <img src="assets/images/lunox-flip.png" alt="hero" class="img-fluid" width="500" height="500">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section style="background-color: #eef2f5;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3" style="margin-top: 30px;">
+                        <h1>Products</h1>
+                        <p style="margin-top: 15px;">
+                            Tempat topup nomor 1 di Indonesia<br>
+                            dengan banyak diskon dan promo menarik<br>
+                            setiap harinya.
+                        </p>
+                        <button class="btn btn-primary" style="margin-top: 15px;">Eksplor Game</button>
+                    </div>
+                    <div class="col-lg-9" style="margin-top: 30px; margin-bottom: 30px;">
+                        <div class="row">
+                            <div class="col-lg-4 d-flex justify-content-center" style="padding: 10px;">
+                                <div class="card" style="width: 12rem;">
+                                    <img src="assets/images/mlbb.png" class="card-img-top" alt="mlbb">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Mobile Legends: Bang Bang</h6>
+                                        <p class="card-text">
+                                            Diskon up to<br>
+                                            Rp 20.000
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 d-flex justify-content-center" style="padding: 10px;">
+                                <div class="card" style="width: 12rem;">
+                                    <img src="assets/images/mlbb.png" class="card-img-top" alt="mlbb">
+                                    <div class="card-body">
+                                        <h6 class="card-title">Mobile Legends: Bang Bang</h6>
+                                        <p class="card-text">
+                                            Diskon up to<br>
+                                            Rp 20.000
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+    </main>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
